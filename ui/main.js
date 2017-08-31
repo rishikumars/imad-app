3a0bc1f1fc33441b6ee90fe906304ffa6a56f2c3
rishikumars <30955992+rishikumars@users.noreply.github.com>
2017-09-01 03:04:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ submit.onclick=function (){
                 
                 var list='';
                 for(var i=0;i<names.length;i++){
-                    list +='<li>'+names[i]+'</li>';
+                    list += '<li>' +names[i]+ '</li>';
                   
                     
                     
